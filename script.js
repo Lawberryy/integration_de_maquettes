049e1d54a9f2c81702fcf6b87d3c724989e89815
Sixtine Delvallée <sixtine.delvallee@gmail.com>
2023-02-17 11:58:44 +0100

    :sparkles: ajout graphique + :construction: responsive

--- script.js
+++ script.js
@@ -1,17 +1,100 @@
-divNumbers = document.getElementById('numbers-col')
+// divNumbers = document.getElementById('numbers-col')
 
-for (i = 1; i <= 7; i++) {
-    let spanNumber = document.createElement('span')
-    spanNumber.innerHTML = '$' + i + '00'
+// for (i = 1; i <= 7; i++) {
+//     let spanNumber = document.createElement('span')
+//     spanNumber.innerHTML = '$' + i + '00'
 
-    divNumbers.appendChild(spanNumber)
-}
+//     divNumbers.appendChild(spanNumber)
+// }
 
-divDays = document.getElementById('days')
+// divDays = document.getElementById('days')
 
-for (i = 5; i <= 15; i++) {
-    let spanDay = document.createElement('span')
-    spanDay.innerHTML = i + 'Nov'
+// for (i = 5; i <= 15; i++) {
+//     let spanDay = document.createElement('span')
+//     spanDay.innerHTML = i + 'Nov'
 
-    divDays.appendChild(spanDay)
-}
+//     divDays.appendChild(spanDay)
+// }
+
+// chart js
+
+let srx = document.getElementById('graph').getContext('2d'); // id de la balise canvas
+// getContext indique que l'on va ajouter un objet dans le canva
+
+let graph = new Chart(srx, {
+    type: 'line',
+    data: {
+        labels: [
+            '5Nov',
+            '6Nov',
+            '7Nov',
+            '8Nov',
+            '9Nov',
+            '10Nov',
+            '11Nov',
+            '12Nov',
+            '13Nov',
+            '14Nov',
+            '15Nov',
+        ],
+        datasets: [
+            {
+                label: 'ETH',
+                data: [
+                    '100',
+                    '400',
+                    '600',
+                    '400',
+                    '300',
+                    '100',
+                    '350',
+                    '200',
+                    '300',
+                    '700',
+                    '600',
+                ],
+                borderColor: [
+                    '#2F7EC1'
+                ],
+            },
+            {
+                label: 'BTC',
+                data: [
+                    'null',
+                    '600',
+                    '200',
+                    '250',
+                    '400',
+                    '300',
+                    '150',
+                    '500',
+                    '200',
+                    '600',
+                    '500',
+                ],
+                borderColor: [
+                    '#FAFF00'
+                ],
+            },
+            {
+                label: 'LTC',
+                data: [
+                    'null',
+                    'null',
+                    'null',
+                    '100',
+                    '300',
+                    '400',
+                    '250',
+                    '700',
+                    '400',
+                    '300',
+                    '400',
+                ],
+                borderColor: [
+                    '#611D8D'
+                ],
+            },
+        ]
+    }
+})
